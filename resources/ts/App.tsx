f78e3298df3a733c26712a1d2a70fea236d7e49b
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-04-29 21:13:25 +0900

    axios-get-method

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => {
-    return <TopPage>トップページ</TopPage>;
+const App: React.VFC = () => {
+    const [tasks, setTasks] = useState([]);
+    const getData = async () => {
+        const jsonData = await axios.get("api/tasks");
+        console.log(jsonData.data);
+        console.log(jsonData.data[0].title);
+        console.log(jsonData.data.map((data) => data));
+        setTasks(jsonData.data.map((data) => data));
+    };
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <ul>
+            {tasks.map((task, key) => (
+                <li key={key}>{task.title}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default App;
-
-const TopPage = styled.div`
-    font-size: 3rem;
-    color: red;
-`;
