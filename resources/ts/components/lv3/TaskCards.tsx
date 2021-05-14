0947c564ac93f314a4675b92e7381f6818c3739d
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-05-14 15:54:29 +0900

    styled-media-query

--- resources/ts/components/lv3/TaskCards.tsx
+++ resources/ts/components/lv3/TaskCards.tsx
@@ -1,8 +1,22 @@
 import styled from "styled-components";
+import customMedia from "../../style/customMedia";
 
 export const TaskCards = styled.div`
     max-width: 1300px;
     width: 85%;
     margin: 0 auto;
     padding-top: 10px;
+    //スマホ
+    ${customMedia.lessThan("mobile")`
+     /* screen width is less than 599px (tablet) */
+    width: 100%;
+    `} //タブレット
+    ${customMedia.between("mobile", "tablet")`
+    /* screen width is between 599px (tablet) and 1024px (desktop) */
+
+    `} //PC
+    ${customMedia.greaterThan("tablet")`
+    /* screen width is greater than 1024px (tablet) */
+    
+    `}
 `;
