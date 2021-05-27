13998737e002c61655e07fca85fd35525955dc6a
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-05-27 16:38:28 +0900

    fix

--- resources/ts/style/ButtonStyle.tsx
+++ resources/ts/style/ButtonStyle.tsx
@@ -5,11 +5,9 @@ export const ButtonStyle = styled.div<{ backgroundColor: string }>`
     text-align: center;
     background-color: ${(props) => props.backgroundColor};
     opacity: 0.9;
-    /* color: #252525; */
     color: white;
     padding: 6px 10px;
     border-radius: 4px;
-    /* border-bottom: 4px solid #627295; */
     margin-right: 12px;
     cursor: pointer;
     &:hover {
