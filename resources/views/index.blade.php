cd5ebf886e1f36f4b4b97abd97fd1ef36c23ffcb
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-03 13:41:36 +0900

    favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="icon/favicon.ico" />
+    <link rel="apple-touch-icon" href="icon/apple-touch-icon.png" sizes="180x180" />
+    <link rel="icon" type="image/png" href="icon/android-touch-icon.png" />
+    
     <meta name='description' content='シンプルなTodoアプリです' >
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
