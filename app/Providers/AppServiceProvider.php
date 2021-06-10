8ddea7a08b5e360edd468ab1cb22c373827f9b6a
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-10 22:38:54 +0900

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $is_production = env('APP_ENV') === 'production' ? true : false;
         View::share('is_production',$is_production);
+
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
