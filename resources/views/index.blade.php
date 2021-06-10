ead32cbae7188a64aa4b3542a36ba392ad842241
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-10 21:26:09 +0900

    ssh

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,15 +18,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js',$is_production) }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="{{ mix('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/app.css'),$is_production) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/style.css'),$is_production) }}">
 </head>
 <body>
             @if (Route::has('login'))
