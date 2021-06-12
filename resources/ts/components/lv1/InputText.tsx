879a3efbe47d437f2e97db9a5d93ac67e8b95031
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-12 14:37:04 +0900

    add

--- resources/ts/components/lv1/InputText.tsx
+++ resources/ts/components/lv1/InputText.tsx
@@ -1,17 +1,39 @@
 import React from "react";
+import { API } from "../../api/API";
 
 type Props = {
     text: string;
     handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    postTask: (postData: API) => void;
+    setText: (param: string) => void;
+    userID?: number;
 };
 
-const InputText: React.VFC<Props> = ({ text, handleChange }: Props) => {
+const InputText: React.VFC<Props> = ({
+    text,
+    handleChange,
+    postTask,
+    setText,
+    userID,
+}: Props) => {
+    const data: API = {
+        user_id: userID,
+        title: text,
+        is_done: 0,
+    };
     return (
         <input
             name="task"
             value={text}
             placeholder="タスクを入力"
             onChange={(e) => handleChange(e)}
+            onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                    e.preventDefault();
+                    postTask(data);
+                    setText("");
+                }
+            }}
             style={{ padding: "5px" }}
         />
     );
