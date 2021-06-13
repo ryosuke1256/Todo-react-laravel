b139d07808dc80609ddaffbc8a777243aa7c7735
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-13 21:50:25 +0900

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
     <!-- favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="icon/favicon.ico" />
     <link rel="apple-touch-icon" href="icon/apple-touch-icon.png" sizes="180x180" />
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="icon/favicon.ico" />
+    <link rel="apple-touch-icon" href="icon/apple-touch-icon.png" sizes="180x180" />
+    <link rel="icon" type="image/png" href="icon/android-touch-icon.png" /
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js',$is_production) }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset(mix('css/app.css'),$is_production) }}">
 </head>
 <body>
     <div id="app">
