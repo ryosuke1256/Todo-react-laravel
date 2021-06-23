e10d7aa2204420a2e96c7a3d5d3b77dcb58ca6ed
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-23 14:48:59 +0900

    tailwindcss-setting

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./resources/views/index.blade.php", "./resources/ts/App.tsx"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
