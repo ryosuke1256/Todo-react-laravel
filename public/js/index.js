1dc601885849f77c1b7da3f5400b06cf6dab4762
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-27 13:46:50 +0900

    refactoring

--- public/js/index.js
+++ public/js/index.js
@@ -4812,6 +4812,22 @@ exports.default = LoginContent;
 "use strict";
 
 
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+
+    return t;
+  };
+
+  return __assign.apply(this, arguments);
+};
+
 var __createBinding = this && this.__createBinding || (Object.create ? function (o, m, k, k2) {
   if (k2 === undefined) k2 = k;
   Object.defineProperty(o, k2, {
@@ -4895,40 +4911,31 @@ var RegisterContent = function RegisterContent() {
     }
   };
 
+  console.log(registerData);
+
   var handleChangeName = function handleChangeName(e) {
-    setRegisterData({
-      name: e.target.value,
-      email: registerData.email,
-      password: registerData.password,
-      password_confirmation: registerData.password
-    });
+    setRegisterData(__assign(__assign({}, registerData), {
+      name: e.target.value
+    }));
   };
 
   var handleChangeEmail = function handleChangeEmail(e) {
-    setRegisterData({
-      name: registerData.name,
-      email: e.target.value,
-      password: registerData.password,
-      password_confirmation: registerData.password
-    });
+    setRegisterData(__assign(__assign({}, registerData), {
+      email: e.target.value
+    }));
   };
 
   var handleChangePassword = function handleChangePassword(e) {
-    setRegisterData({
-      name: registerData.name,
-      email: registerData.email,
-      password: e.target.value,
-      password_confirmation: e.target.value
-    });
-  };
+    setRegisterData(__assign(__assign({}, registerData), {
+      password: e.target.value
+    }));
+  }; //prettier-ignore
+
 
   var handleChangeConfirmPassword = function handleChangeConfirmPassword(e) {
-    setRegisterData({
-      name: registerData.name,
-      email: registerData.email,
-      password: registerData.password,
+    setRegisterData(__assign(__assign({}, registerData), {
       password_confirmation: e.target.value
-    });
+    })); //prettier-ignore
   };
 
   var togglePassword = function togglePassword(password) {
--- resources/ts/components/lv3/RegisterContent.tsx
+++ resources/ts/components/lv3/RegisterContent.tsx
@@ -27,43 +27,23 @@ const RegisterContent: React.VFC = () => {
             console.log("確認のパスワードが一致しません");
         }
     };
+    console.log(registerData);
 
     const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setRegisterData({
-            name: e.target.value,
-            email: registerData.email,
-            password: registerData.password,
-            password_confirmation: registerData.password,
-        });
+        setRegisterData({ ...registerData, name: e.target.value });
     };
 
     const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setRegisterData({
-            name: registerData.name,
-            email: e.target.value,
-            password: registerData.password,
-            password_confirmation: registerData.password,
-        });
+        setRegisterData({ ...registerData, email: e.target.value });
     };
 
     const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setRegisterData({
-            name: registerData.name,
-            email: registerData.email,
-            password: e.target.value,
-            password_confirmation: e.target.value,
-        });
+        setRegisterData({ ...registerData, password: e.target.value });
     };
 
-    const handleChangeConfirmPassword = (
-        e: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setRegisterData({
-            name: registerData.name,
-            email: registerData.email,
-            password: registerData.password,
-            password_confirmation: e.target.value,
-        });
+    //prettier-ignore
+    const handleChangeConfirmPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRegisterData({ ...registerData, password_confirmation: e.target.value }); //prettier-ignore
     };
 
     const togglePassword = (password: string) => {
