af477f03c8fc64981ef270672e7b3b5a206d463a
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-06-27 09:04:13 +0900

    User registration function

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::Resource('api/tags','TagController');
 Route::get('api/tags/tasks/{id}','TagController@index');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LogoutController::class, 'logout']);
+Route::post('/register',[RegisterController::class, 'register']);
 
 Route::get('/login', function () {
         return redirect('/');
